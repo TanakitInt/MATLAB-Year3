f884605828f5685a3fe47b9c2da64644b5ae09f9
TanakitInt <imTanakit@gmail.com>
2020-05-13 21:15:07 +0700

    Deep Learning code fix

--- EXTRA - Deep Learning/CameraAndNetwork.m
+++ EXTRA - Deep Learning/CameraAndNetwork.m
@@ -4,11 +4,11 @@ close all;
 camera = webcam;    % Connect to the camera
 nnet = alexnet;     % Load the neural net
 
-while True
+while true
     picture = camera.snapshot;              % Take a picture
     picture = imresize(picture, [227, 227]) % Resize the picture
     
-    label = classifiy(nnet, picture);       % Classify the picture
+    label = classify(nnet, picture);       % Classify the picture
     
     image(picture);     % Show the picture
     title(char(label)); % Show the label
